2fcbd40ec42268b7c6904b92b6359067ab891401
EduLen <107079321+EduLen@users.noreply.github.com>
2023-04-11 22:20:22 -0600

    Tables Updated
    All tables changed

--- sangre/Tablas.sql
+++ sangre/Tablas.sql
@@ -7,8 +7,6 @@ DROP DATABASE IF EXISTS RegistroDonantesSangre;
 Create DataBase RegistroDonantesSangre;
 Use RegistroDonantesSangre;
 
-
--- DROP TABLE IF EXISTS Datos_Personales;
 Create Table Datos_Personales( 
 	id_Datos_Personales INT NOT NULL AUTO_INCREMENT,
 	cedula Varchar (16) not null UNIQUE,
@@ -18,8 +16,7 @@ Create Table Datos_Personales(
 	primary key (id_Datos_Personales)
 );
 
-SELECT *
-FROM Datos_Personales;
+Select * from Datos_Personales;
 
 Create Table Datos_Sangre(
 id_Tipo_Sangre int not null AUTO_INCREMENT,
@@ -28,35 +25,32 @@ tipo_Sangre VARCHAR (3) not null,
 primary key (id_Tipo_Sangre),
 foreign key(id_Persona) references Datos_Personales(id_Datos_Personales)
 );
-SELECT * FROM Datos_Sangre;
 
 CREATE TABLE Departamento(
-	id_Departamento INT NOT NULL auto_increment,
-	id_Municipio INT NOT NULL,
-	id_Centro_Donacion INT NOT NULL,
+	id_Departamento INT NOT NULL auto_increment,	
 	departamento VARCHAR(255),
-	PRIMARY KEY(id_Departamento),
-	FOREIGN KEY(id_Municipio) REFERENCES Municipio(id_Municipio),
-	FOREIGN KEY(id_Centro_Donacion) REFERENCES Centro_Donacion(id_Centro_Donacion)
+	PRIMARY KEY(id_Departamento)
 );
-SELECT * FROM Departamento;
+
 
 CREATE TABLE Municipio(
 	id_Municipio INT NOT NULL auto_increment,
-	id_Centro_Donacion INT NOT NULL,
+	id_Departamento int not NULL,
 	municipio VARCHAR(255),
 	PRIMARY KEY(id_Municipio),
-	FOREIGN KEY(id_Centro_Donacion) REFERENCES Centro_Donacion(id_Centro_Donacion)
+	FOREIGN KEY(id_Departamento) REFERENCES Departamento(id_Departamento)
 );
-SELECT * FROM Municipio;
 
 CREATE TABLE Centro_Donacion(
 	id_Centro_Donacion INT NOT NULL auto_increment,
+	id_Departamento int NOT NULL,
+	id_Municipio INT NOT NULL,
 	centro_Donacion varchar(255),
-	PRIMARY KEY(id_Centro_Donacion)
+	PRIMARY KEY(id_Centro_Donacion),
+	FOREIGN KEY(id_Departamento) REFERENCES Departamento(id_Departamento),
+	FOREIGN KEY(id_Municipio) REFERENCES Municipio(id_Municipio)
 );
 
-SELECT * FROM Centro_Donacion;
 
 CREATE TABLE Donacion(
 	id_Donacion INT AUTO_INCREMENT NOT NULL, 
@@ -71,13 +65,6 @@ CREATE TABLE Donacion(
     foreign key (id_Centro_Donacion) references Centro_Donacion (id_Centro_Donacion)
 );
 
-SELECT * From Donacion;
-	-- este id_Datos_Personales de donante, nosotros en c# vamos a tener por ejemplo un datagrid con la lista de personas, 
-	-- vamos a hacer una busqueda de las personas, y una vez que seleccionemos esa persona, obtenemos su id_Datos_Personales
--- manejarla cantidad en ml
-
--- _____________________________________________
--- arreglar y relacionar
 CREATE TABLE Almacen (
 	id_Bolsa int not null auto_increment,
 	id_Donacion int NOT NULL,
@@ -87,7 +74,7 @@ CREATE TABLE Almacen (
    Foreign Key (id_Donacion) REFERENCES Donacion(id_Donacion),
    foreign key (id_Tipo_Sangre) references Datos_Sangre(id_Tipo_Sangre)
 );
-Drop table if EXISTS Almacen;
+
 CREATE TABLE Exportacion(
 	id_Exportacion INT NOT NULL auto_increment,
 	id_Bolsa int NOT NULL,
@@ -98,6 +85,3 @@ CREATE TABLE Exportacion(
 	PRIMARY KEY(id_Exportacion),
 	Foreign Key (id_Bolsa) REFERENCES Almacen(id_Bolsa)
 );
--- arreglar y relacionar
--- _____________________________________________
- show tables;
